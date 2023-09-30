766afb9004a5d58a02eed61c079f6cbedd461e27
mahzumy <mahzumy39@gmail.com>
2023-09-30 13:27:36 +0700

    final edit

--- src/app/api/v1/login/route.js
+++ src/app/api/v1/login/route.js
@@ -1,4 +1,4 @@
-import { prisma } from "@/app/utils/prisma";
+import { prisma } from "@/utils/prisma";
 import { NextResponse } from "next/server";
 import bcrypt from "bcrypt"
 import { sign } from "jsonwebtoken";
